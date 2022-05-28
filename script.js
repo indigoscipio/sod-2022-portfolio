ff67ce49ff546b972e827373e281e1ca2b36315e
unknown <samueloctavianus97@gmail.com>
2022-05-28 17:44:41 +0700

    completed css

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ let btnMenu = document.querySelector("#icon-menu");
 let btnClose = document.querySelector("#icon-close");
 let btnContainer = document.querySelector(".side-nav__menu-icon");
 let menuContainer = document.querySelector(".menu-container");
+let sliderContainer = document.querySelector(".testimonials__slider-container");
+let sliderIndicators = document.querySelectorAll(".slider-indicator");
 
 btnContainer.addEventListener("click", toggleMenu);
+
+let slideIndex = 1;
+
+function autoRotate() {
+  sliderIndicators.forEach((element) => {
+    element.classList.remove("active");
+  });
+}
 
 function toggleMenu(e) {
   console.log(e.target.id);
